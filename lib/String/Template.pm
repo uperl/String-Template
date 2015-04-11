e16c5714f19c9bb0de138e0686157f53940ebc6e
Graham Ollis <gollis@sesda2.com>
2015-04-11 11:00:00 -0500

    migrate to Dist::Zilla

--- lib/String/Template.pm
+++ lib/String/Template.pm
@@ -2,15 +2,33 @@ package String::Template;
 
 use strict;
 use warnings;
-
 use base 'Exporter';
 use POSIX;
 use Date::Parse;
 
+# ABSTRACT: Fills in string templates from hash of fields
+# VERSION
+
+=head1 SYNOPSIS
+
+  use String::Template;
+
+  my %fields = ( num => 2, str => 'this', date => 'Feb 27, 2008' );
+
+  my $template = "...<num%04d>...<str>...<date:%Y/%m/%d>...\n";
+
+  print expand_string($template, \%fields);
+
+  prints: "...0002...this...2008/02/27..."
+
+=head1 DESCRIPTION
+
+Generate strings based on a template.
+
+=cut
+
 our @EXPORT = qw(expand_string missing_values expand_stringi);
 our @EXPORT_OK = qw(expand_hash);
-
-our $VERSION = '0.16';
 
 my %special =
 (
@@ -50,6 +68,40 @@ sub _replace
                                 : ($undef_flag ? "<$field>" : '');
 }
 
+=head1 FUNCTIONS
+
+=head2 expand_string
+
+ my $str = expand_string($template, \%fields, $undef_flag);
+
+Fills in a simple template with values from a hash, replacing tokens
+like "<fieldname>" with the value from the hash $fields->{fieldname}.
+
+Some special characters can be used to impose formatting on the
+fields:
+
+ % - treat like a sprintf() format
+     e.g.  <int%02d>
+
+ : - treat like a L<POSIX::strftime()> format
+     e.g. <date:%Y-%m-%d>
+
+ ! - Just like ':', but with gmtime instead of localtime
+     e.g. <gmdate!%Y-%m-%d %H:%M>
+
+ # - treat like args to substr()
+     e.g. <str#0,2> or <str#4>
+
+For the ':' strftime formats, the field is parsed by L<Date::Parse>,
+so it can handle any format that can handle.
+
+Handling of undefined fields can be controlled with $undef_flag.  If
+it is false (default), undefined fields are simply replace with an
+empty string.  If set to true, the field is kept verbatim.  This can
+be useful for multiple expansion passes.
+
+=cut
+
 #
 # expand_string($string, \%fields, $undef_flag)
 # find "<fieldname>"
@@ -63,6 +115,15 @@ sub expand_string
     return $string;
 }
 
+=head2 expand_stringi
+
+ my $str = expand_stringi($template, \%fields, $undef_flag);
+
+expand_stringi works just like expand_string, except that tokens
+and hash keys are treated case insensitively.
+
+=cut
+
 sub expand_stringi
 {
     my ($string, $fields, $undef_flag) = @_;
@@ -72,6 +133,18 @@ sub expand_stringi
 
     return $string;
 }
+
+=head2 missing_values
+
+ my @missing = missing_values($template, \%fields, $dont_allow_undefs);
+
+Checks to see if the template variables in a string template exist
+in a hash.  Set $dont_allow_undefs to 1 to also check to see if the
+values for all such keys are defined.
+
+Returns a list of missing keys or an empty list if no keys were missing.
+
+=cut
 
 sub missing_values
 {
@@ -85,6 +158,17 @@ sub missing_values
     return unless @missing;
     return @missing;
 }
+
+=head2 $status = expand_hash($hash[, $maxdepth]);
+
+Expand a hash of templates/values.  This function will repeatedly
+replace templates in the values of the hash with the values of the
+hash they reference, until either all "<>" templates are gone, or
+it has iterated $maxdepth times (default 10).
+
+Returns undef if there are unexpanded templates left, otherwise true.
+
+=cut
 
 sub expand_hash
 {
@@ -116,100 +200,11 @@ sub expand_hash
 }
 
 1;
-__END__
-
-=head1 NAME
-
-String::Template - Fills in string templates from hash of fields
-
-=head1 SYNOPSIS
-
-  use String::Template;
-
-  my %fields = ( num => 2, str => 'this', date => 'Feb 27, 2008' );
-
-  my $template = "...<num%04d>...<str>...<date:%Y/%m/%d>...\n";
-
-  print expand_string($template, \%fields);
-
-  prints: "...0002...this...2008/02/27..."
-
-=head1 DESCRIPTION
-
-=head2 $str = expand_string($template, \%fields, $undef_flag).
-
-Fills in a simple template with values from a hash, replacing tokens
-like "<fieldname>" with the value from the hash $fields->{fieldname}.
-
-Some special characters can be used to impose formatting on the
-fields:
-
- % - treat like a sprintf() format
-     e.g.  <int%02d>
-
- : - treat like a L<POSIX::strftime()> format
-     e.g. <date:%Y-%m-%d>
-
- ! - Just like ':', but with gmtime instead of localtime
-     e.g. <gmdate!%Y-%m-%d %H:%M>
-
- # - treat like args to substr()
-     e.g. <str#0,2> or <str#4>
-
-For the ':' strftime formats, the field is parsed by L<Date::Parse>,
-so it can handle any format that can handle.
-
-Handling of undefined fields can be controlled with $undef_flag.  If
-it is false (default), undefined fields are simply replace with an
-empty string.  If set to true, the field is kept verbatim.  This can
-be useful for multiple expansion passes.
-
-=head2 $str = expand_stringi($template, \%fields, $undef_flag).
-
-expand_stringi works just like expand_string, except that tokens
-and hash keys are treated case insensitively.
-
-=head2 @missing = missing_values($template, \%fields, $dont_allow_undefs)
-
-Checks to see if the template variables in a string template exist
-in a hash.  Set $dont_allow_undefs to 1 to also check to see if the
-values for all such keys are defined.
-
-Returns a list of missing keys or an empty list if no keys were missing.
-
-=head2 $status = expand_hash($hash[, $maxdepth]);
-
-Expand a hash of templates/values.  This function will repeatedly
-replace templates in the values of the hash with the values of the
-hash they reference, until either all "<>" templates are gone, or
-it has iterated $maxdepth times (default 10).
-
-Returns undef if there are unexpanded templates left, otherwise true.
-
-=head1 AUTHOR
-
-=over 4
-
-=item Current maintainer: Graham Ollis <plicease@cpan.org>
-
-=item Brian Duggan
-
-=item Curt Tilmes
-
-=item Jeremy Mates (thirg, JMATES)
-
-=back
 
 =head1 SEE ALSO
 
 L<String::Format> performs a similar function, with a different
 syntax.
-
-=head1 COPYRIGHT AND LICENSE
-
-This software is cpyright 2015 by Brian Duggan
-
-This is free software; you can redistribute it and/or modify it under the same terms as the Perl 5 programming language system itself.
 
 =cut
 
