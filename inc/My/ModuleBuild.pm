e4c8f37ad6b4d8a096944464ee9e623d24983954
Graham Ollis <plicease@cpan.org>
2015-04-13 13:19:03 -0400

    on windows we need Time::Piece

---
+++ inc/My/ModuleBuild.pm
@@ -0,0 +1,18 @@
+package My::ModuleBuild;
+
+use strict;
+use warnings;
+use base qw( Module::Build );
+
+sub new
+{
+  my($class, %args) = @_;
+
+  $args{test_requires}->{'Time::Piece'} = '1.17' if $^O eq 'MSWin32';
+  
+  my $self = $class->SUPER::new(%args);
+  
+  $self;
+}
+
+1;
